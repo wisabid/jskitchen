20c2e6c13b44b9488c8310dd08c09ca1864646f4
Abid Shamim PM <fanboy@Abids-MacBook-Pro.local>
2020-06-23 12:22:44 +0100

    fix(logo): update

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ import headerStyles from "../styles/components/header.module.scss"
 export default function Header(props) {
   return (
     <header
-      className={`${headerStyles.header} ${props.page === 'info' &&
+      className={`${headerStyles.header} ${props.page === "info" &&
         headerStyles.info_page}`}
     >
       <nav
@@ -14,21 +14,19 @@ export default function Header(props) {
         aria-label="main navigation"
       >
         <Link to="/">
-          <h1>{props.title}</h1>
+          {/* <h1>{props.title}</h1> */}
+          <img
+            src="https://yt3.ggpht.com/a/AATXAJyQAptT6uy6ENvPfMRZLB6tVrDHbZeDf0QJ8A=s288-c-k-c0xffffffff-no-rj-mo"
+            width="100"
+          />
         </Link>
         <div>
           <h1>
             <Link
-              to={
-                props.page === 'info'
-                  ? "/"
-                  : "/info"
-              }
+              to={props.page === "info" ? "/" : "/info"}
               activeClassName={headerStyles.navItemActive}
             >
-              {props.page === 'info'
-                ? "close"
-                : "info"}
+              {props.page === "info" ? "close" : "info"}
             </Link>
           </h1>
         </div>
